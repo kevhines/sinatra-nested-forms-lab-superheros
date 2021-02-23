406d5437965513a2050099bf61593dac0cbb6137
Kevin <kevhines@gmail.com>
2021-02-23 16:13:09 -0500

    commit tree 00ea1eb1724b654a8f061797a9e4a6dde7f6b67e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :team
+    end
+
+    post '/teams' do
+        puts params
+        @team_hash = params["team"]
+        puts @team_hash
+        erb :super_hero
+    end
+
 
 end
